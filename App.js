422cdcfd3c91ce4f6d00cb9742e7b76b6424b5aa
Seiji Ohara <DanielSOhara27@users.noreply.github.com>
2021-06-23 15:44:41 -0500

    Returning to initial state

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {Image, SafeAreaView, StyleSheet, Text} from 'react-native';
 
 export default function App() {
 
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text >Hello React Native</Text>
+      <Text >Hello React Native
+        {/*<Image source={require('./assets/icon.png')}></Image> This is how you link a static image from your assets folder*/}
+      </Text>
+      <Image
+          fadeDuration={1000} /*Only works on Android*/
+          source={{
+        width:200,
+        height:200,
+        uri:"https://picsum.photos/200"}}>
+        {/*In React Native, network images need to have their dimensions specified or they will not be rendered*/}
+      </Image>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
