e4fa293f59b12dc036209c5d06d1030252732fbd
Seiji Ohara <DanielSOhara27@users.noreply.github.com>
2021-06-23 13:22:05 -0500

    Text Lesson - React Native Fundamentals

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log("Text component clicked");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Hello React Native - A really really long text. Now I wanna to write even more text to see what happens</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
